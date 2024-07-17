1237a19512fd8c85a14a297138f89010efd2bd4d
EnolaZhu <85802657+EnolaZhu@users.noreply.github.com>
2024-07-17 14:46:20 +0800

    [1.0.0] Create project and import RIBs with SPM

---
+++ Counter with RIBs/Counter with RIBs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter with RIBs
+//
+//  Created by enola.zhu on 2024/7/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
